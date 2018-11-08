0f8bc1917e05315b0f0d91928214bc20b41b0a51
CarmenLoor <carmenk74@hotmail.com>
2018-11-08 16:49:58 -0500

    Mas informacion: cambios al layout de mas informacion agregar iconos y mejorar la presentacion

--- app/src/main/java/es/codigoandroid/geoturse/MasInformacion.java
+++ app/src/main/java/es/codigoandroid/geoturse/MasInformacion.java
@@ -3,6 +3,7 @@ package es.codigoandroid.geoturse;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.ScrollingMovementMethod;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -14,6 +15,8 @@ import es.codigoandroid.pojos.Costo;
 import es.codigoandroid.pojos.Facilidad;
 import es.codigoandroid.pojos.Idiomas;
 import es.codigoandroid.pojos.Recursos;
+import es.codigoandroid.pojos.TipoAccesibilidad;
+import es.codigoandroid.pojos.TipoAtractivo;
 
 /**
  * Created by carme_000 on 14/06/2017.
@@ -21,7 +24,8 @@ import es.codigoandroid.pojos.Recursos;
 public class MasInformacion extends AppCompatActivity {
     CouchbaseManager<String, Recursos> dbaRecurso;
     public Recursos recursoAlmacenado;
-    public TextView tv_tipoRecurso,tv_accesibilidad,tv_acceso,tv_Infraestructura,tv_parqueos,tv_facilidades,tv_seguridad,tv_idiomas;
+    public TextView tv_tipoRecurso,tv_accesibilidad,tv_acceso,tv_Infraestructura,
+            tv_parqueos,tv_facilidades,tv_seguridad,tv_idiomas;
 
 
     @Override
@@ -57,6 +61,7 @@ public class MasInformacion extends AppCompatActivity {
         ArrayList<Idiomas> idioR=recursoAlmacenado.getIdiomasInformac();
         ArrayList<AccesibilidadRecurso> accR=recursoAlmacenado.getOpcionesAccesibilidad();
         ArrayList<Costo> costoR=recursoAlmacenado.getCostoRecursos();
+        ArrayList<String> tiposParqueos =recursoAlmacenado.getTiposParqueo();
 
         tv_facilidades.setText(" ");
         tv_accesibilidad.setText(" ");
@@ -68,26 +73,71 @@ public class MasInformacion extends AppCompatActivity {
         tv_tipoRecurso.setText(" ");
 
 
-        if(recursoAlmacenado.getCategoria()!= null)
+        if(recursoAlmacenado.getCategoria()!= null) {
+            tv_tipoRecurso.setMovementMethod(new ScrollingMovementMethod());
             tv_tipoRecurso.setText(recursoAlmacenado.getCategoria());
+        }else
+            tv_tipoRecurso.setText("Sin información");
 
-        if(recursoAlmacenado.getSeguridad()!= null)
+
+        if(recursoAlmacenado.getSeguridad()!= null) {
+            tv_seguridad.setMovementMethod(new ScrollingMovementMethod());
             tv_seguridad.setText(recursoAlmacenado.getSeguridad());
+        }else
+            tv_tipoRecurso.setText("Sin información");
 
-        if(facR.size()>0)
-        for(Facilidad fa: facR)
-            if(fa!=null)
-                tv_facilidades.setText(tv_facilidades.getText().toString()+"\n"+fa.getTitulo()+"\n"+fa.getDescripcion());
+        if(facR!=null) {
+            tv_facilidades.setMovementMethod(new ScrollingMovementMethod());
+            for (Facilidad fa : facR) {
+                if (fa != null)
+                    tv_facilidades.setText(tv_facilidades.getText().toString() + "\n" + fa.getTitulo() + "\n" + fa.getDescripcion());
+            }
 
-        if(idioR.size()>0)
-        for(Idiomas idi: idioR)
-            if(idi!=null)
-                tv_idiomas.setText(tv_idiomas.getText()+"\n"+idi.name());
-/*        if(accR.size()>0)
-        for(AccesibilidadRecurso ac: accR)
-            if(ac!=null)
-                tv_accesibilidad.setText(tv_accesibilidad.getText()+"\n"+ac.getTitulo()+": "+ac.getDescripcion());
-*/
+        }else
+            tv_tipoRecurso.setText("Sin información");
+
+
+        if(idioR!=null){
+        for(Idiomas idi: idioR) {
+            tv_idiomas.setMovementMethod(new ScrollingMovementMethod());
+            if (idi != null)
+                tv_idiomas.setText(tv_idiomas.getText() + "\n" + idi.name());
+            }
+        } else
+            tv_idiomas.setText("Sin información");
+
+
+        if(accR != null) {
+            tv_accesibilidad.setMovementMethod(new ScrollingMovementMethod());
+            for (AccesibilidadRecurso ac : accR) {
+                if (ac != null)
+                    tv_accesibilidad.setText(tv_accesibilidad.getText() + "\n" + ac.getTitulo() + ": " + ac.getDescripcion());
+            }
+        }else
+            tv_accesibilidad.setText("Sin información");
+
+
+        if(recursoAlmacenado.getAcceso()!=null) {
+            tv_acceso.setMovementMethod(new ScrollingMovementMethod());
+            tv_acceso.setText(recursoAlmacenado.getAcceso());
+        }else
+            tv_acceso.setText("Sin información");
+
+        if(recursoAlmacenado.getInfraestructura()!=null) {
+            tv_Infraestructura.setMovementMethod(new ScrollingMovementMethod());
+            tv_Infraestructura.setText(recursoAlmacenado.getInfraestructura());
+        }else
+            tv_Infraestructura.setText("Sin información");
+
+        if(tiposParqueos!=null) {
+            tv_parqueos.setMovementMethod(new ScrollingMovementMethod());
+            for (String tipoParqueo : tiposParqueos) {
+                if (tipoParqueo != null) {
+                    tv_parqueos.setText(tv_parqueos.getText() + "\n" + tipoParqueo);
+                }
+            }
+        }else
+            tv_parqueos.setText("Sin información");
 
     }
 
@@ -97,7 +147,6 @@ public class MasInformacion extends AppCompatActivity {
         onBackPressed();
         return false;
     }
-
 
 
 }
